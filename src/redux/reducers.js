2a32b4f8a7c4014188f5e34144eac5f7b9833261
ali chatraeii <avida.chatraee2001@yahoo.com>
2021-10-15 10:34:59 +0300

    Create reducers - rootReducer & store redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,16 @@
+const initialState = {
+  users: [],
+  user: {},
+  loading: false,
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "":
+      break;
+
+    default:
+      return state;
+  }
+};
+export { usersReducer };
